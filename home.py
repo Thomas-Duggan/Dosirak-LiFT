c2deecf54d5789ced5427c82ea6ed1ba8846d2cd
Thomas Duggan <duggantaw@gmail.com>
2025-09-29 10:53:16 -0230

    Changed name references

--- home.py
+++ home.py
@@ -1,6 +1,6 @@
 #Home#
 
-# Simple Linux Utility and Toolbox  Copyright (c) 2025  Thomas Duggan
+# Dosirak LiFT  Copyright (c) 2025  Thomas Duggan
 # This work is licensed under CC BY-SA 4.0
 
 background = "light blue"
@@ -20,30 +20,25 @@ prd.text(f"{[x for x in range (17)]}",
          color = background,
          background = background)
 
-prd.text("Welcome to...",
-         type = "title_not_bold",
-         background = background,
-         location = "bottom")
-
-prd.text("Simplified",
+prd.text("Welcome to Dosirak:",
          type = "home_title",
-         background = background,
          color = "purple",
+         background = background,
          location = "bottom")
 
-prd.text("Linux Utilities",
-         type = "home_title",
+prd.text("Linux Fixes",
+         type = "title_not_bold",
          background = background,
          color = "blue",
          location = "bottom")
 
-prd.text("and Toolbox",
-         type = "home_title",
+prd.text("and Tricks",
+         type = "title_not_bold",
          background = background,
          color = "dark blue",
          location = "bottom")
 
-prd.text ("Last updated Sept 4, 2025",
+prd.text ("Last updated Sept 29, 2025",
           type = "subtitle",
           background = background,
           color = "grey",
@@ -68,7 +63,7 @@ prd.group_open(parent = prd.root,
                background = background)
 
 prd.button("Info",
-           lambda:(prd.run('gnome-terminal -- python3 ~/simplified-linux-utilities-and-toolbox/modules/key_modules/info.py && exit', shell = True), exit()),
+           lambda:(prd.run('gnome-terminal -- python3 ~/dosirak-lift/modules/key_modules/info.py && exit', shell = True), exit()),
            keep_row = True)
 
 prd.button("Quit",
@@ -76,7 +71,7 @@ prd.button("Quit",
            keep_row = True)
 
 prd.button("Settings",
-           lambda:(prd.run('gnome-terminal -- python3 ~/simplified-linux-utilities-and-toolbox/modules/key_modules/settings.py && exit', shell = True), exit()))
+           lambda:(prd.run('gnome-terminal -- python3 ~/dosirak-lift/modules/key_modules/settings.py && exit', shell = True), exit()))
 
 prd.group_close(sticky = "n")
 
