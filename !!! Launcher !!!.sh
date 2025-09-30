a4f3f396ef307473c15b2b7d90f989722646738d
Thomas Duggan <duggantaw@gmail.com>
2025-09-30 14:16:03 -0230

    Added launcher script

---
+++ !!! Launcher !!!.sh
@@ -0,0 +1,5 @@
+# Simplified Linux Utilities and Toolbox  Copyright (c) 2025  Thomas Duggan
+# This work is licensed under CC BY-SA 4.0
+
+cd ~/simplified-linux-utilities-and-toolbox
+python3 home.py
